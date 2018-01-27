83ab5fec136a4ee323e14e0c66b3f5db5ac2411b
Deesthortered <anatoly.davydko@gmail.com>
2018-01-27 18:37:18 +0200

    Priorty queues are updated

--- My containers C++/PriorityQueue.h
+++ My containers C++/PriorityQueue.h
@@ -4,7 +4,7 @@
 namespace spacePriorityQueue
 {
 	template <class T1, class T2>
-	class PriorityQueue
+	class PriorityQueueMin
 	{
 		struct Pair
 		{
@@ -16,10 +16,20 @@ namespace spacePriorityQueue
 		spaceArray::Array<Pair> arr;
 
 	public:
-		PriorityQueue() = default;
-		~PriorityQueue()
+		PriorityQueueMin() = default;
+		PriorityQueueMin(const PriorityQueueMin &obj)
+		{
+			this->arr = obj.arr;
+		}
+		~PriorityQueueMin()
 		{
 			this->arr.~Array();
+		}
+
+		PriorityQueueMin& operator=(const PriorityQueueMin &obj)
+		{
+			this->arr = obj.arr;
+			return *this;
 		}
 
 		bool IsEmpty()
@@ -77,6 +87,100 @@ namespace spacePriorityQueue
 			}
 		}
 		T1	 ExtractMin()
+		{
+			if (this->arr.IsEmpty()) throw "Empty queue";
+			return this->arr[0].data;
+		}
+		void ShrinkToFit()
+		{
+			this->arr.ShrinkToFit();
+		}
+	};
+
+	template <class T1, class T2>
+	class PriorityQueueMax
+	{
+		struct Pair
+		{
+			T1 data;
+			T2 key;
+			Pair() = default;
+			Pair(T1 _data, T2 _key) : data(_data), key(_key) {}
+		};
+		spaceArray::Array<Pair> arr;
+
+	public:
+		PriorityQueueMax() = default;
+		PriorityQueueMax(const PriorityQueueMax &obj)
+		{
+			this->arr = obj.arr;
+		}
+		~PriorityQueueMax()
+		{
+			this->arr.~Array();
+		}
+
+		PriorityQueueMax& operator=(const PriorityQueueMax &obj)
+		{
+			this->arr = obj.arr;
+			return *this;
+		}
+
+		bool IsEmpty()
+		{
+			return (this->arr.IsEmpty() ? true : false);
+		}
+		void Push(T1 data, T2 key)
+		{
+			int i = this->arr.Size();
+			this->arr.Push_back(Pair(data, key));
+			while (i && this->arr[i].key > this->arr[(i - 1) >> 1].key)
+			{
+				swap(this->arr[(i - 1) >> 1], this->arr[i]);
+				i = (i - 1) >> 1;
+			}
+		}
+		void Pop()
+		{
+			if (this->arr.IsEmpty()) return;
+			size_t sz = this->arr.Size() - 1;
+			size_t i = 0;
+
+			this->arr[0] = this->arr[sz];
+			this->arr.Pop_back();
+
+			while ((i << 1) + 1 < sz)
+			{
+				if ((i << 1) + 2 < sz)
+				{
+					if (this->arr[(i << 1) + 1].key > this->arr[(i << 1) + 2].key)
+					{
+						if (this->arr[(i << 1) + 1].key > this->arr[i].key)
+						{
+							swap(this->arr[(i << 1) + 1], this->arr[i]);
+							i = (i << 1) + 1;
+						}
+						else break;
+					}
+					else
+					{
+						if (this->arr[(i << 1) + 2].key > this->arr[i].key)
+						{
+							swap(this->arr[(i << 1) + 2], this->arr[i]);
+							i = (i << 1) + 2;
+						}
+						else break;
+					}
+				}
+				else if (this->arr[(i << 1) + 1].key > this->arr[i].key)
+				{
+					swap(this->arr[(i << 1) + 1], this->arr[i]);
+					i = (i << 1) + 1;
+				}
+				else break;
+			}
+		}
+		T1	 ExtractMax()
 		{
 			if (this->arr.IsEmpty()) throw "Empty queue";
 			return this->arr[0].data;
--- My containers C++/main.cpp
+++ My containers C++/main.cpp
@@ -1,17 +1,11 @@
 #include <vld.h>
 #include <iostream>
-#include "Red_Black_Tree.h"
+#include "PriorityQueue.h"
 using namespace std;
 
 int main()
 {
-	spaceRed_Black_Tree::Red_Black_Tree<int> tree;
 
-	tree.Insert(1);
-	tree.Insert(2);
-	tree.Insert(3);
-
-	spaceRed_Black_Tree::Red_Black_Tree<int> tree1(tree);
 
 	system("pause");
 	return 0;
