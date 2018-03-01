8d97dac32d15cc8296a866571294a9801d8981c7
Deesthortered <anatoly.davydko@gmail.com>
2018-03-01 19:01:03 +0200

    Start to create template of graph.

---
+++ My containers C++/Graph.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Array.h"
+
+namespace spaceGraph
+{
+	template<class Vertex>
+	class Graph
+	{
+		spaceArray::Array<Vertex> arr;
+
+	public:
+		Graph()
+		{
+
+		}
+		~Graph()
+		{
+
+		}
+	};
+}
